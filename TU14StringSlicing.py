37bf0a634b4187bed75f43dffce6592b77a01b0d
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-08-29 07:35:52 +0800

    Create TU14StringSlicing.py
    AS String Slicing Exercise

---
+++ TU14StringSlicing.py
@@ -0,0 +1,9 @@
+#Exercise 1: Setup variable and print it out
+myString = "abcdef"
+print(myString)
+
+#Exercise 2: Come up with your own variable and slice it forwards, backwards and skip a letter or two.
+myString = "theepilogue"
+print(myString[1:5])
+print(myString[-1:-8:-1])
+print(myString[-2:-9:-2])
