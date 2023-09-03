722559bd8dbc94d2fa70400cc2046b68353a3d8f
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-09-04 07:35:03 +0800

    Create TU01Print.py

---
+++ TU01Print.py
@@ -0,0 +1,8 @@
+#Exercise 1: Print your own phrase
+print("I really like listening to music!")
+
+#Exercise 2: Try doing the most complicated Maths you can in your head and compare it to the answers you get from the computer.
+print((-2)**2 + 5*2/50 -928*(58**3+2))
+
+#Exercise 3: Try 22/7  What do you get?  
+print(22/7)
