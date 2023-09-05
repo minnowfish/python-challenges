fbe95016a9a8d4ce28410e62ad1397f460fac133
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-09-05 08:42:26 +0800

    Create AS40ClearScreen.py

---
+++ AS40ClearScreen.py
@@ -0,0 +1,12 @@
+clear = False
+def clearScreen():
+    for i in range(50):
+        print(" ")
+
+while clear == False:
+    print("dgy2wou8ijdhbhehiuwskjndeiwsknaxmb dchgeyfudiwijsknxbdcvghefyqudhwksjna") 
+    userInput= input("Would you like to clear the screen? (y/n)")
+    if userInput.lower() == "y":
+        break
+
+clearScreen()
