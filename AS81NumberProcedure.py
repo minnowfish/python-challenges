0c8ec66dbcdc7278809e7a423bc5f326724e87d3
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2024-02-26 21:17:58 +0800

    Update AS81NumberProcedure.py

--- AS81NumberProcedure.py
+++ AS81NumberProcedure.py
@@ -1,9 +1,11 @@
 def sum(numbers):
     total = 0
+    product = 1
     for i in numbers:
         total += i
+        product *= i
 
-    print(f"Sum of all numbers is: {total}")
+    print(f"Sum of all numbers is {total} and product is {product}")
 
 numbers = [int(x) for x in input("Input numbers separated by space: ").split()]
 sum(numbers)
